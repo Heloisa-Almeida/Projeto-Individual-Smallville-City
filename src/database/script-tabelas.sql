ee5c808db878b352cd0f12f6200bfd67cbc66614
Heloisa-Almeida <heloisa.a200607@gmail.com>
2026-05-31 13:49:07 -0300

    Alterações em comentários

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -20,3 +20,27 @@ CREATE TABLE caracteristicas (
     invulnerabilidade DECIMAL(5,2)
 );
 
+insert into caracteristicas values
+(DEFAULT, 'Superman', 70.99, 98.50, 100.00, 85.75, 38.40, 69.20);
+
+CREATE TABLE superman (
+    idSuperman INT PRIMARY KEY AUTO_INCREMENT,
+    nomeHeroi VARCHAR(50),
+    nomeReal VARCHAR(50),
+    planetaOrigem VARCHAR(50),
+    fraqueza VARCHAR(50)
+);
+
+INSERT INTO superman (nomeHeroi, nomeReal, planetaOrigem, fraqueza) VALUES
+('Superman', 'Clark Kent', 'Krypton', 'Kryptonita');
+
+ALTER TABLE caracteristicas ADD COLUMN fkSuperman INT;
+ALTER TABLE caracteristicas
+ADD CONSTRAINT fkCaracteristicaSuperman
+FOREIGN KEY (fkSuperman)
+REFERENCES superman(idSuperman);
+UPDATE caracteristicas
+SET fkSuperman = 1
+WHERE id = 1;
+
+
